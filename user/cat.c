847c6ba1f349844a9494b0ab867e211ed31f32aa
Abdul Haseeb <ahhqhaseeb@gmail.com>
2022-11-22 21:37:54 +0500

    second commit

--- user/cat.c
+++ user/cat.c
@@ -30,7 +30,29 @@ main(int argc, char *argv[])
     cat(0);
     exit(0);
   }
-
+if(strcmp(argv[1], "-n") == 0)// "==" compares pointers but strcmp compares t>
+  {
+        for (i=0;i<argc;i++)
+        { printf(" %d",i);
+//      fd = open(while ((c = getc(fp)) != EOF) {     /* read each character */
+ //       if (last)                       /* test if last set */
+   //         putchar (last);             /* output all characters */
+     //   if (last == '\n')               /* test if last is newline */
+       //     printf ("%06zu ", ln++);    /* output next line number */
+       // last = c;
+        }
+        for (i=2;i<argc;i++)
+        {
+         if((fd = open(argv[i], 0)) < 0){
+      fprintf(2, "cat: cannot open %s\n", argv[i]);
+      exit(1);
+        }
+         cat(fd);
+	close(fd);
+	}
+}
+else
+{
   for(i = 1; i < argc; i++){
     if((fd = open(argv[i], 0)) < 0){
       fprintf(2, "cat: cannot open %s\n", argv[i]);
@@ -38,6 +60,11 @@ main(int argc, char *argv[])
     }
     cat(fd);
     close(fd);
-  }
-  exit(0);
+        }
+         cat(fd);
+
+
+ exit(0);
+}
+return 0;
 }
