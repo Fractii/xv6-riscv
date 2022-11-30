bfdc1a072ec72b429091b4c9722f062ae4b9525f
Fractii <umerkhan180701@gmail.com>
2022-12-01 00:59:28 +0500

    Third Milestone first commit

---
+++ user/sort.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+int 
+main(void) {
+    printf("return val of system call is %d\n", sort());
+    printf("Congrats !! You have successfully added new system  call in xv6 OS :) \n");
+    exit(0);
+ }
